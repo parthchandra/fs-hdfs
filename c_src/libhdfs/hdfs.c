4cddfb710f032675973e9b7e5944158aa29b4e1b
Parth Chandra <parthc@apple.com>
2025-07-14 15:51:34 -0700

    Support additional file systems

--- c_src/libhdfs/hdfs.c
+++ c_src/libhdfs/hdfs.c
@@ -57,7 +57,7 @@
 // Bit fields for hdfsFile_internal flags
 #define HDFS_FILE_SUPPORTS_DIRECT_READ (1<<0)
 
-tSize readDirect(hdfsFS fs, hdfsFile f, void* buffer, tSize length);
+tSize readDirect(hdfsFS fs, hdfsFile f, void* buffer, tSize length, int noPrintFlag);
 static void hdfsFreeFileInfoEntry(hdfsFileInfo *hdfsFileInfo);
 
 /**
@@ -1006,7 +1006,7 @@ hdfsFile hdfsOpenFile(hdfsFS fs, const char *path, int flags,
     if ((flags & O_WRONLY) == 0) {
         // Try a test read to see if we can do direct reads
         char buf;
-        if (readDirect(fs, file, &buf, 0) == 0) {
+        if (readDirect(fs, file, &buf, 0, NOPRINT_UNSUPPORTED_OPERATION) == 0) {
             // Success - 0-byte read should return 0
             file->flags |= HDFS_FILE_SUPPORTS_DIRECT_READ;
         } else if (errno != ENOTSUP) {
@@ -1231,7 +1231,7 @@ tSize hdfsRead(hdfsFS fs, hdfsFile f, void* buffer, tSize length)
         return -1;
     }
     if (f->flags & HDFS_FILE_SUPPORTS_DIRECT_READ) {
-      return readDirect(fs, f, buffer, length);
+      return readDirect(fs, f, buffer, length, PRINT_EXC_ALL);
     }
 
     // JAVA EQUIVALENT:
@@ -1286,7 +1286,7 @@ tSize hdfsRead(hdfsFS fs, hdfsFile f, void* buffer, tSize length)
 }
 
 // Reads using the read(ByteBuffer) API, which does fewer copies
-tSize readDirect(hdfsFS fs, hdfsFile f, void* buffer, tSize length)
+tSize readDirect(hdfsFS fs, hdfsFile f, void* buffer, tSize length, int noPrintFlags)
 {
     // JAVA EQUIVALENT:
     //  ByteBuffer bbuffer = ByteBuffer.allocateDirect(length) // wraps C buffer
@@ -1320,7 +1320,7 @@ tSize readDirect(hdfsFS fs, hdfsFile f, void* buffer, tSize length)
         HADOOP_ISTRM, "read", "(Ljava/nio/ByteBuffer;)I", bb);
     destroyLocalReference(env, bb);
     if (jthr) {
-        errno = printExceptionAndFree(env, jthr, PRINT_EXC_ALL,
+        errno = printExceptionAndFree(env, jthr, noPrintFlags,
             "readDirect: FSDataInputStream#read");
         return -1;
     }
